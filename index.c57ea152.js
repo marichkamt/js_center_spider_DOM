c314d6235007c6bb8489dede76764a3fef168a65
marichkamt <emilycry1999@gmail.com>
2024-09-13 12:43:37 +0200

    make build

---
+++ index.c57ea152.js
@@ -0,0 +1,2 @@
+document.querySelector(".spider").style.cssText="\n  top: 50%;\n  left: 50%;\n  margin: -25px 0 0 -25px;\n";
+//# sourceMappingURL=index.c57ea152.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const spider = document.querySelector(".spider");
+spider.style.cssText = `
+  top: 50%;
+  left: 50%;
+  margin: -25px 0 0 -25px;
+`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
